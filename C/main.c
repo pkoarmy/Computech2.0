65894f3c409aac2bfb19d0ae399df8008c3c86d7
Ray Park <pkoarmy@yahoo.co.kr>
2020-08-01 21:47:27 +0000

    added sizeof

--- C/main.c
+++ C/main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+
 #define NOTE_C 244
 
 void play(int d) {
@@ -8,5 +9,12 @@ void play(int d) {
 int main(void) {
   printf("Hello World\n");
   play(5);
+
+  printf(" char size : %lu \n", sizeof(char));
+  printf(" char size : %lu \n", sizeof(int));
+  printf(" char size : %lu \n", sizeof(unsigned int));
+  printf(" char size : %lu \n", sizeof(float));
+  printf(" char size : %lu \n", sizeof(long));
+  printf(" char size : %lu \n", sizeof(unsigned long));
   return 0;
 }
